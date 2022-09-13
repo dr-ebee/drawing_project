b86720e4b64ccbc8ab0bc472a7c88d011b0c5591
Erin Bennett <erinbennett@Erins-MacBook-Air.local>
2022-09-13 10:28:44 -0700

    remove animation

--- starter_code.py
+++ starter_code.py
@@ -1,6 +1,14 @@
 from turtle import *
 window = Screen()
+
+# This line and the update() line at the end skip the animation
+tracer(0, 0)
+
 ##### start your code here ####
  
 ##### end your code here #####
+
+# This line and the tracer(0, 0) line at the beginning skip the animation
+update()
+
 window.exitonclick()
